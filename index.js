8438e043518bcb9234d5760ecf5073f7e049bcfb
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2024-09-22 20:47:48 +0200

    add new pictures to main site, start with javascript, add the modal

---
+++ index.js
@@ -0,0 +1,21 @@
+// Získání elementů
+const modal = document.getElementById('myIntro');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementsByClassName('close')[0];
+
+// Otevření modalu při kliknutí na tlačítko
+openModalBtn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+// Zavření modalu při kliknutí na "X"
+closeModalBtn.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// Zavření modalu při kliknutí mimo obsah okna
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
