1c0b95ca93b5407d42302c6614c3554de0ee352c
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2024-09-23 20:13:05 +0200

    style the courses page, add images, style the modal

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/*Hamburger menu */
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
 // Získání elementů
 const modal = document.getElementById('myIntro');
 const openModalBtn = document.getElementById('openModalBtn');
