d2f83324396d0a3caea899569490b4101cfe6f3f
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2024-10-05 19:12:11 +0200

    finish the about us site

--- index.js
+++ index.js
@@ -2,29 +2,34 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
-
+// Kontrola, zda hamburger existuje na stránce
+if (hamburger && navLinks) {
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+}
 
 // Získání elementů
 const modal = document.getElementById('myIntro');
 const openModalBtn = document.getElementById('openModalBtn');
 const closeModalBtn = document.getElementsByClassName('close')[0];
 
-// Otevření modalu při kliknutí na tlačítko
-openModalBtn.onclick = function () {
-  modal.style.display = 'block';
-};
+// Kontrola, zda modal existuje na stránce
+if (modal && openModalBtn && closeModalBtn) {
+  // Otevření modalu při kliknutí na tlačítko
+  openModalBtn.onclick = function () {
+    modal.style.display = 'block';
+  };
 
-// Zavření modalu při kliknutí na "X"
-closeModalBtn.onclick = function () {
-  modal.style.display = 'none';
-};
-
-// Zavření modalu při kliknutí mimo obsah okna
-window.onclick = function (event) {
-  if (event.target == modal) {
+  // Zavření modalu při kliknutí na "X"
+  closeModalBtn.onclick = function () {
     modal.style.display = 'none';
-  }
-};
+  };
+
+  // Zavření modalu při kliknutí mimo obsah okna
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
+}
