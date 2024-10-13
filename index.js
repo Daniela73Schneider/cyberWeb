b1b3826acc4d1aa85cb2097688f41436af959f29
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2024-10-13 20:35:27 +0200

    style button and modal

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*Hamburger menu */
+/* Hamburger menu */
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
 
@@ -9,27 +9,53 @@ if (hamburger && navLinks) {
   });
 }
 
-// Získání elementů
-const modal = document.getElementById('myIntro');
-const openModalBtn = document.getElementById('openModalBtn');
-const closeModalBtn = document.getElementsByClassName('close')[0];
+// Získání elementů pro modální okna
+const modalInfo = document.getElementById('myIntro');
+const modalPrice = document.getElementById('myPrice');
+const openModalInfoBtn = document.getElementById('openModalInfoBtn');
+const openModalPriceBtn = document.getElementById('openModalPriceBtn');
+const closeModalInfoBtn = modalInfo.querySelector('.close');
+const closeModalPriceBtn = modalPrice.querySelector('.close');
 
-// Kontrola, zda modal existuje na stránce
-if (modal && openModalBtn && closeModalBtn) {
-  // Otevření modalu při kliknutí na tlačítko
-  openModalBtn.onclick = function () {
-    modal.style.display = 'block';
+// Inicializace modálních oken na skrytý stav
+modalInfo.style.display = 'none';
+modalPrice.style.display = 'none';
+
+// Kontrola, zda modální okna existují na stránce
+if (modalInfo && openModalInfoBtn && closeModalInfoBtn) {
+  // Otevření modalu při kliknutí na tlačítko pro informace
+  openModalInfoBtn.onclick = function () {
+    modalInfo.style.display = 'flex'; // Použijeme flex pro vertikální a horizontální centrování
   };
 
   // Zavření modalu při kliknutí na "X"
-  closeModalBtn.onclick = function () {
-    modal.style.display = 'none';
+  closeModalInfoBtn.onclick = function () {
+    modalInfo.style.display = 'none';
   };
 
   // Zavření modalu při kliknutí mimo obsah okna
   window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = 'none';
+    if (event.target === modalInfo) {
+      modalInfo.style.display = 'none';
+    }
+  };
+}
+
+if (modalPrice && openModalPriceBtn && closeModalPriceBtn) {
+  // Otevření modalu při kliknutí na tlačítko pro ceník
+  openModalPriceBtn.onclick = function () {
+    modalPrice.style.display = 'flex'; // Použijeme flex pro vertikální a horizontální centrování
+  };
+
+  // Zavření modalu při kliknutí na "X"
+  closeModalPriceBtn.onclick = function () {
+    modalPrice.style.display = 'none';
+  };
+
+  // Zavření modalu při kliknutí mimo obsah okna
+  window.onclick = function (event) {
+    if (event.target === modalPrice) {
+      modalPrice.style.display = 'none';
     }
   };
 }
